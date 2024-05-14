ae7f4a2b90a054499bae5de7623d4183dd8d7de3
Rohan <rohanahire651@gmail.com>
2024-05-14 15:27:56 +0530

    Shorted the address, changed the table header bg color

--- components/AllFunders.tsx
+++ components/AllFunders.tsx
@@ -1,43 +1,47 @@
 import React from "react";
 
 const AllFunders = () => {
+  const address = "0x128733E87074F39D2b77E5e1101C2047CC553cb0";
+  const shortenedAddress = `${address.substring(0, 10)}....${address.substring(
+    address.length - 10
+  )}`;
   return (
     <div className=" mx-5">
-      <div className=" flex items-center justify-between rounded-[10px]  border-[3px] py-[12px] border-slate-300">
-        <div className=" flex items-center gap-x-[7px] text-[17px] font-bold mx-5">
+      <div className="flex md:flex-row flex-col items-start md:items-center justify-between rounded-[10px]  border-[3px] py-[12px] border-slate-300 gap-y-3 px-5">
+        <div className=" flex md:flex-row flex-col items-start md:items-center gap-x-[7px] text-[17px] font-bold mx-5">
           <p className=" text-gray-500">Current Funds:</p>
           <p>0 ETH</p>
         </div>
-        <div className=" flex items-center gap-x-[7px] text-[17px] font-bold mx-5">
+        <div className=" flex md:flex-row flex-col items-start md:items-center gap-x-[7px] text-[17px] font-bold mx-5">
           <p className=" text-gray-500">Contract Add:</p>
           <a
             href="https://etherscan.io/address/0x128733E87074F39D2b77E5e1101C2047CC553cb0"
             className=" underline font-semibold hover:text-indigo-400"
             target="_blank"
           >
-            0x128733E87074F39D2b77E5e1101C2047CC553cb0
+            {shortenedAddress}
           </a>
         </div>
-        <div className=" flex items-center gap-x-[7px] text-[17px] font-bold mx-5">
+        <div className=" flex md:flex-row flex-col items-start md:items-center gap-x-[7px] text-[17px] font-bold mx-5">
           <p className=" text-gray-500">Owner Add:</p>
           <a
             href="https://etherscan.io/address/0x128733E87074F39D2b77E5e1101C2047CC553cb0"
             target="_blank"
             className=" underline font-semibold hover:text-indigo-400"
           >
-            0x128733E87074F39D2b77E5e1101C2047CC553cb0
+            {shortenedAddress}
           </a>
         </div>
       </div>
 
-      <div className=" flex flex-col justify-center items-center mt-3">
+      <div className=" hidden md:flex flex-col justify-center items-center mt-3">
         <table className=" w-full mx-5">
           <thead className="p-3 text-white text-[19px]">
             <tr>
-              <th className=" rounded-tl-[10px] py-3 bg-[#211951]">No.</th>
-              <th className=" py-3 bg-[#211951]">Funders</th>
-              <th className=" py-3 bg-[#211951]">Fund</th>
-              <th className=" rounded-tr-[10px] py-3 bg-[#211951]">Date</th>
+              <th className=" rounded-tl-[10px] py-3 bg-[#151515]">No.</th>
+              <th className=" py-3 bg-[#151515]">Funders</th>
+              <th className=" py-3 bg-[#151515]">Fund</th>
+              <th className=" rounded-tr-[10px] py-3 bg-[#151515]">Date</th>
             </tr>
           </thead>
 
@@ -50,7 +54,7 @@ const AllFunders = () => {
                   target="_blank"
                   className=" underline hover:text-indigo-400"
                 >
-                  0x128733E87074F39D2b77E5e1101C2047CC553cb0
+                  {address}
                 </a>
               </td>
               <td className=" py-3 border-b-2 border-zinc-300">1 ETH</td>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,13 @@ import { FaEthereum } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className=" flex justify-between items-center bg-[#151515] w-full py-3 px-5">
-      <div className=" bg-[#151515] flex items-center text-[23px] font-extrabold gap-x-1 text-white">
+    <div className=" flex justify-between items-center bg-[#151515] w-full py-4 px-5">
+      <div className=" bg-[#151515] flex items-center text-[25px] font-extrabold gap-x-1 text-white">
         FundFusion{" "}
-        <FaEthereum className="bg-[#151515] text-indigo-400 text-[25px]" />
+        <FaEthereum className="bg-[#151515] text-indigo-400 text-[26px]" />
       </div>
       <div className=" rounded-[10px]">
-        <button className=" py-[7px] px-5 font-semibold rounded-[10px] hover:scale-105 ">
+        <button className=" py-[9px] px-5 font-semibold rounded-[10px] hover:scale-105 ">
           Connect
         </button>
       </div>
